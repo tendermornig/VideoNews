29644c8b58025c425934cd3d0f83994ad59e51e7
陈龙 <2802660864@qq.com>
2021-01-07 13:08:09 +0800

    将视频接口与资讯接口分开来

--- src/main/java/service/NewsService.java
+++ src/main/java/service/NewsService.java
@@ -15,7 +15,7 @@ import java.util.List;
  * @author 光
  */
 @RestController
-@RequestMapping("/list")
+@RequestMapping("/news")
 public class NewsService {
 
     /**
@@ -27,7 +27,7 @@ public class NewsService {
      * @return 访问结果
      */
     @ResponseBody
-    @RequestMapping(value = "/news", params = {"limit", "page"}, method = RequestMethod.GET)
+    @RequestMapping(value = "/list", params = {"limit", "page"}, method = RequestMethod.GET)
     public BaseResponse<List<NewsModel>> getNewsList(int limit, long page, @RequestHeader("token") String token) {
         BaseResponse<List<NewsModel>> result = new BaseResponse<>();
         if (TokenUtil.verificationToken(token)) {
@@ -91,93 +91,6 @@ public class NewsService {
                     newsThumbModels.add(newsThumbModel);
                 }
                 result.setData(newsThumbModels);
-            } else {
-                result.setCode(ApiConfig.FAIL_CODE_403);
-            }
-        } catch (SQLException troubles) {
-            troubles.printStackTrace();
-        } finally {
-            DbUtil.Instance.close();
-        }
-        return result;
-    }
-
-    /**
-     * 获取所有视频类别的接口
-     *
-     * @param token 验证的token
-     * @return 访问结果
-     */
-    @ResponseBody
-    @RequestMapping(value = "/category", method = RequestMethod.GET)
-    public BaseResponse<List<VideoCategoryModel>> getCategory(@RequestHeader("token") String token) {
-        BaseResponse<List<VideoCategoryModel>> result = new BaseResponse<>();
-        if (TokenUtil.verificationToken(token)) {
-            result.setMsg(ApiConfig.SUCCESS);
-            result.setCode(ApiConfig.FAIL_CODE_402);
-            return result;
-        }
-        String sql = "select * from video_category";
-        ResultSet rs = DbUtil.Instance.queryDb(sql);
-        try {
-            if (rs != null) {
-                List<VideoCategoryModel> categoryList = new ArrayList<>();
-                while (rs.next()) {
-                    VideoCategoryModel categoryModel = new VideoCategoryModel();
-                    categoryModel.setCategoryId(rs.getInt(1));
-                    categoryModel.setCategoryName(rs.getString(2));
-                    categoryList.add(categoryModel);
-                }
-                result.setMsg(ApiConfig.SUCCESS);
-                result.setCode(ApiConfig.SUCCESS_CODE_200);
-                result.setData(categoryList);
-            } else {
-                result.setCode(ApiConfig.FAIL_CODE_403);
-            }
-        } catch (SQLException troubles) {
-            troubles.printStackTrace();
-        } finally {
-            DbUtil.Instance.close();
-        }
-        return result;
-    }
-
-    /**
-     * 获取视频的接口
-     *
-     * @param token    验证的token
-     * @param category 本次访问的视频类别
-     * @return 访问结果
-     */
-    @ResponseBody
-    @RequestMapping(value = "/video", params = "category", method = RequestMethod.GET)
-    public BaseResponse<List<VideoModel>> getVideoList(@RequestHeader("token") String token, int category) {
-        BaseResponse<List<VideoModel>> result = new BaseResponse<>();
-        if (TokenUtil.verificationToken(token)) {
-            result.setMsg(ApiConfig.SUCCESS);
-            result.setCode(ApiConfig.FAIL_CODE_402);
-            return result;
-        }
-        String sql = "select * from video_list where category_id = " + category + "";
-        ResultSet rs = DbUtil.Instance.queryDb(sql);
-        try {
-            if (rs != null) {
-                List<VideoModel> categoryList = new ArrayList<>();
-                while (rs.next()) {
-                    VideoModel videoModel = new VideoModel();
-                    videoModel.setVtitle(rs.getString(2));
-                    videoModel.setAuthor(rs.getString(3));
-                    videoModel.setCoverUrl(rs.getString(4));
-                    videoModel.setHeadurl(rs.getString(5));
-                    videoModel.setCommentNum(rs.getInt(6));
-                    videoModel.setLikeNum(rs.getInt(7));
-                    videoModel.setCollectNum(rs.getInt(8));
-                    videoModel.setPlayUrl(rs.getString(9));
-                    categoryList.add(videoModel);
-                }
-                result.setMsg(ApiConfig.SUCCESS);
-                result.setCode(ApiConfig.SUCCESS_CODE_200);
-                result.setData(categoryList);
             } else {
                 result.setCode(ApiConfig.FAIL_CODE_403);
             }
---
+++ src/main/java/service/VideoService.java
@@ -0,0 +1,109 @@
+package service;
+
+import modles.BaseResponse;
+import modles.VideoCategoryModel;
+import modles.VideoModel;
+import org.springframework.web.bind.annotation.*;
+import utils.ApiConfig;
+import utils.DbUtil;
+import utils.TokenUtil;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Miracle
+ */
+@RestController
+@RequestMapping("/video")
+public class VideoService {
+
+    /**
+     * 获取视频的接口
+     *
+     * @param token    验证的token
+     * @param category 本次访问的视频类别
+     * @return 访问结果
+     */
+    @ResponseBody
+    @RequestMapping(value = "/list", params = "category", method = RequestMethod.GET)
+    public BaseResponse<List<VideoModel>> getVideoList(@RequestHeader("token") String token, int category) {
+        BaseResponse<List<VideoModel>> result = new BaseResponse<>();
+        if (TokenUtil.verificationToken(token)) {
+            result.setMsg(ApiConfig.SUCCESS);
+            result.setCode(ApiConfig.FAIL_CODE_402);
+            return result;
+        }
+        String sql = "select * from video_list where category_id = " + category + "";
+        ResultSet rs = DbUtil.Instance.queryDb(sql);
+        try {
+            if (rs != null) {
+                List<VideoModel> categoryList = new ArrayList<>();
+                while (rs.next()) {
+                    VideoModel videoModel = new VideoModel();
+                    videoModel.setVtitle(rs.getString(2));
+                    videoModel.setAuthor(rs.getString(3));
+                    videoModel.setCoverUrl(rs.getString(4));
+                    videoModel.setHeadurl(rs.getString(5));
+                    videoModel.setCommentNum(rs.getInt(6));
+                    videoModel.setLikeNum(rs.getInt(7));
+                    videoModel.setCollectNum(rs.getInt(8));
+                    videoModel.setPlayUrl(rs.getString(9));
+                    categoryList.add(videoModel);
+                }
+                result.setMsg(ApiConfig.SUCCESS);
+                result.setCode(ApiConfig.SUCCESS_CODE_200);
+                result.setData(categoryList);
+            } else {
+                result.setCode(ApiConfig.FAIL_CODE_403);
+            }
+        } catch (SQLException troubles) {
+            troubles.printStackTrace();
+        } finally {
+            DbUtil.Instance.close();
+        }
+        return result;
+    }
+
+    /**
+     * 获取所有视频类别的接口
+     *
+     * @param token 验证的token
+     * @return 访问结果
+     */
+    @ResponseBody
+    @RequestMapping(value = "/category", method = RequestMethod.GET)
+    public BaseResponse<List<VideoCategoryModel>> getCategory(@RequestHeader("token") String token) {
+        BaseResponse<List<VideoCategoryModel>> result = new BaseResponse<>();
+        if (TokenUtil.verificationToken(token)) {
+            result.setMsg(ApiConfig.SUCCESS);
+            result.setCode(ApiConfig.FAIL_CODE_402);
+            return result;
+        }
+        String sql = "select * from video_category";
+        ResultSet rs = DbUtil.Instance.queryDb(sql);
+        try {
+            if (rs != null) {
+                List<VideoCategoryModel> categoryList = new ArrayList<>();
+                while (rs.next()) {
+                    VideoCategoryModel categoryModel = new VideoCategoryModel();
+                    categoryModel.setCategoryId(rs.getInt(1));
+                    categoryModel.setCategoryName(rs.getString(2));
+                    categoryList.add(categoryModel);
+                }
+                result.setMsg(ApiConfig.SUCCESS);
+                result.setCode(ApiConfig.SUCCESS_CODE_200);
+                result.setData(categoryList);
+            } else {
+                result.setCode(ApiConfig.FAIL_CODE_403);
+            }
+        } catch (SQLException troubles) {
+            troubles.printStackTrace();
+        } finally {
+            DbUtil.Instance.close();
+        }
+        return result;
+    }
+}
